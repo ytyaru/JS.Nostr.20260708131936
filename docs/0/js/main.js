233cef7045807b2f5fc7597175ddb9a3216daf66
ytyaru <yttry0@gmail.com>
2026-07-08 15:56:00 +0900

    Created a repository

---
+++ docs/0/js/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOMContentLoaded!!');
+    const author = 'ytyaru';
+    van.add(document.querySelector('main'), 
+        van.tags.h1(van.tags.a({href:`https://github.com/${author}/JS.Nostr.20260708131936/`}, 'Nostr')),
+        van.tags.p('Nostrを始めてみる。'),
+//        van.tags.p('Get started with Nostr.'),
+    );
+    van.add(document.querySelector('footer'),  new Footer('ytyaru', '../').make());
+
+    const a = new Assertion();
+    a.t(true);
+    a.f(false);
+    a.e(TypeError, `msg`, ()=>{throw new TypeError(`msg`)});
+    a.fin();
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log('beforeunload!!');
+});
+
